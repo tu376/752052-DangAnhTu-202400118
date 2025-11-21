48e2b5842ae09cdb677b3eb64875fd412fdc1894
Dang Anh Tu <dangtu@gmail.com>
2025-11-21 21:38:20 +0700

    Add

--- AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -4,13 +4,11 @@ public class DigitalVideoDisc extends Media {
 
     private String director;
     private int length;
-    private static int nbDigitalVideoDiscs = 0;
-
+    
     public DigitalVideoDisc(int id, String title, String category, String director, int length, float cost) {
         super(id, title, category, cost);
         this.director = director;
         this.length = length;
-        setId(++nbDigitalVideoDiscs);
     }
 
     public String getDirector() {
